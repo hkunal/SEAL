ec08241aff768d0cf83dd3604175b4ef62d883ae
harish kunal <harishkunal@gmail.com>
2017-03-07 11:00:45 -0800

    Add project files.

---
+++ SEALproject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SEALproject.Startup))]
+namespace SEALproject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
